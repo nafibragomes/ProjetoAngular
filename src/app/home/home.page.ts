3e5471cf7f3f282735879d8d73a6b4427d93c076
nafibragomes <125035401+nafibragomes@users.noreply.github.com>
2023-04-28 16:12:52 +0000

    condição dos cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,16 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [
+    IonicModule,
+    CommonModule
+  
+  ],
 })
 export class HomePage {
-  constructor() {}
+  item_selecionado = 'brasil';
+ selecionarCards(event: any){
+  this.item_selecionado = event.detail.value;
+  console.log(event.detail.value);
+ }
 }
